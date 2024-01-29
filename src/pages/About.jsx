0f6008f8c1770f464dfae1cb2f57f8e1a303c687
cptlazy1 <o.culhaci@gmail.com>
2024-01-29 20:10:49 +0100

    Update styling due to change in font

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,26 +6,56 @@ function About() {
             <h1>About</h1>
             <div className="about-inner-container">
 
-                <p>Welcome to the 'About Us' page of Retro Game Jockey, the ultimate platform for retro gaming
-                    enthusiasts. Born out of a deep love for the golden age of gaming, Retro Game Jockey is more than
-                    just a platform - it's a community. Our mission is to provide a space where gamers can manage their
-                    retro game collections, discover hidden gems, and connect with like-minded individuals.
-                </p>
-                <p>
-                    Retro Game Jockey was created by a team of passionate gamers who understand the joy of rediscovering
-                    a classic game and the thrill of adding a rare find to your collection. We believe that every game
-                    has a story to tell, and we're here to help you tell it. Our platform allows you to organize your
-                    collection, track your favorite games, and share your experiences with a community that shares your
-                    passion.
-                </p>
-                <p>But Retro Game Jockey is not just for seasoned collectors. If you're new to the world of retro
-                    gaming, you'll find a wealth of resources to help you get started. From discovering the classics to
-                    understanding the value of rare games, we're here to guide you on your journey.
-                </p>
-                <p>
-                    Join us today and start your nostalgic journey into the rich history of gaming.
-                    Retro Game Jockey - where the past meets the present, and gamers of all ages come together.
-                </p>
+                {/*<p>Welcome to the 'About Us' page of Retro Game Jockey, the ultimate platform for retro gaming*/}
+                {/*    enthusiasts. Born out of a deep love for the golden age of gaming, Retro Game Jockey is more than*/}
+                {/*    just a platform - it's a community. Our mission is to provide a space where gamers can manage their*/}
+                {/*    retro game collections, discover hidden gems, and connect with like-minded individuals.*/}
+                {/*</p>*/}
+                {/*<p>*/}
+                {/*    Retro Game Jockey was created by a team of passionate gamers who understand the joy of rediscovering*/}
+                {/*    a classic game and the thrill of adding a rare find to your collection. We believe that every game*/}
+                {/*    has a story to tell, and we're here to help you tell it. Our platform allows you to organize your*/}
+                {/*    collection, track your favorite games, and share your experiences with a community that shares your*/}
+                {/*    passion.*/}
+                {/*</p>*/}
+                {/*<p>But Retro Game Jockey is not just for seasoned collectors. If you're new to the world of retro*/}
+                {/*    gaming, you'll find a wealth of resources to help you get started. From discovering the classics to*/}
+                {/*    understanding the value of rare games, we're here to guide you on your journey.*/}
+                {/*</p>*/}
+                {/*<p>*/}
+                {/*    Join us today and start your nostalgic journey into the rich history of gaming.*/}
+                {/*    Retro Game Jockey - where the past meets the present, and gamers of all ages come together.*/}
+                {/*</p>*/}
+
+                <ul>
+                    <li>Welcome to the 'About Us' page of Retro Game Jockey, the ultimate platform for retro gaming
+                        enthusiasts. Born out of a deep love for the golden age of gaming, Retro Game Jockey is more
+                        than
+                        just a platform - it's a community. Our mission is to provide a space where gamers can manage
+                        their
+                        retro game collections, discover hidden gems, and connect with like-minded individuals.
+                    </li>
+                    <li>
+                        Retro Game Jockey was created by a team of passionate gamers who understand the joy of
+                        rediscovering
+                        a classic game and the thrill of adding a rare find to your collection. We believe that every
+                        game
+                        has a story to tell, and we're here to help you tell it. Our platform allows you to organize
+                        your
+                        collection, track your favorite games, and share your experiences with a community that shares
+                        your
+                        passion.
+                    </li>
+                    <li>But Retro Game Jockey is not just for seasoned collectors. If you're new to the world of retro
+                        gaming, you'll find a wealth of resources to help you get started. From discovering the classics
+                        to
+                        understanding the value of rare games, we're here to guide you on your journey.
+                    </li>
+                    <li>
+                        Join us today and start your nostalgic journey into the rich history of gaming.
+                        Retro Game Jockey - where the past meets the present, and gamers of all ages come together.
+                    </li>
+                </ul>
 
 
             </div>
